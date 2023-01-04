935ea94694223756644644df1c0e2729a7681e9b
Guilherme Borges <g.lopesborges@gresb.com>
2023-01-04 16:54:40 +0100

    Remove not needed code from bash file

--- deploy.sh
+++ deploy.sh
@@ -2,16 +2,14 @@
 
 # Login to Kubernetes Cluster.
 UPDATE_KUBECONFIG_COMMAND="aws eks --region ${AWS_REGION} update-kubeconfig --name ${CLUSTER_NAME}"
+
 if [ -n "$CLUSTER_ROLE_ARN" ]; then
     UPDATE_KUBECONFIG_COMMAND="${UPDATE_KUBECONFIG_COMMAND} --role-arn=${CLUSTER_ROLE_ARN}"
 fi
 ${UPDATE_KUBECONFIG_COMMAND}
 
-# Helm Dependency Update
-helm dependency update ${DEPLOY_CHART_PATH:-helm/}
-
 # Helm Deployment
-UNINSTALL_COMMAND="helm uninstall --wait --atomic --timeout ${TIMEOUT}"
+UNINSTALL_COMMAND="helm uninstall --wait --timeout ${TIMEOUT}"
 if [ -n "$DEPLOY_NAMESPACE" ]; then
     UNINSTALL_COMMAND="${UNINSTALL_COMMAND} -n ${DEPLOY_NAMESPACE}"
 fi
